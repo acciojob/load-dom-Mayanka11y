34d578911c8ac0ad4b41c527fb39e5bbbd76c404
Mayanka11y <mayankraj418890@gmail.com>
2025-03-24 10:47:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,4 @@
-
-// Wait for the DOM to be fully loaded
-document.addEventListener("DOMContentLoaded", () => {
-  // Log the success message to the console
-  console.log("DOM load success");
-});
-
-  // Append the text node to the body
-  document.body.appendChild(textNode);
-});
+//your JS code here. If required.
+ document.addEventListener("DOMContentLoaded", function () {
+            document.body.innerText = "DOM load success";
+        });
